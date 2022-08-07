735b7f62d174d3693e46fc8767c5ea7374d27d0f
daniel <eduardo.daniel.s.m@gmail.com>
2022-08-07 12:31:48 -0400

    fix: Fix linting

--- ff2mpv.py
+++ ff2mpv.py
@@ -14,7 +14,12 @@ def main():
 
     args = []
 
-    if ".torrent" in url or "magnet:" in url or "webtorrent://" in url or "peerflix://" in url:
+    if (
+            ".torrent" in url
+            or "magnet:" in url
+            or "webtorrent://" in url
+            or "peerflix://" in url
+    ):
         args = ["webtorrent", "--quiet", "--mpv", url]
         if platform.system() == "Windows":
             args = ["cmd", "/c"] + args
