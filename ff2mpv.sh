977bb75f9816fe1b9a91e57ea358fc98e4dd5471
Eduardo Sanchez <eduardo.sanchez@accedo.tv>
2024-05-04 16:58:58 -0400

    fix: Missing mpv in path if installed with nix

--- ff2mpv.sh
+++ ff2mpv.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 # These variables may be set by nix to launch the browser but for some reason
 # they are inherit thtough native-messagin-host. Uset them to avoid issues
@@ -7,4 +7,11 @@
 # unset LD_PRELOAD
 unset LD_LIBRARY_PATH
 
-python3 ff2mpv.py
+# script_path="$(readlink -f "$0")"
+# script_dir="$(dirname "$script_path")"
+
+[ -d "$HOME/.nix-profile/bin" ] && export PATH="$PATH:$HOME/.nix-profile/bin"
+# exec python3 "$script_dir/ff2mpv.py"
+
+exec python3 ff2mpv.py
+
